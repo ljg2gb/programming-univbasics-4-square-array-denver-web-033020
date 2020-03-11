227b81771ff5eb1d0c9ce41318b43050ffeafccb
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-11 15:18:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
   counter = 0
   while array[counter] do
-    squared = []
-    squared.push(array[counter]*array[counter])
+    array[counter]= (array[counter]**2)
     counter += 1
   end
-  p squared
+  p array
 end
